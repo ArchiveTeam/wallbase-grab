d2e33dfa06289d07e0cbb23481e07127a321fba8
Arkiver2 <Arkiver@hotmail.com>
2014-08-26 16:51:05 +0200

    wallbase.lua: download next and previous urls

--- wallbase.lua
+++ wallbase.lua
@@ -25,6 +25,32 @@ end
 --   end
 -- end
 
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  --example url: http://wallbase.cc/wallpaper/2816669
+  if string.match(url, "wallbase%.cc/wallpaper/") then
+    if not html then
+      html = read_file(file)
+    end
+    
+    --example line: <a href="http://wallbase.cc/wallpaper/go/2816669/prev?ref=aHR0cDovL3dhbGxiYXNlLmNjL2NvbGxlY3Rpb24vMjkwNTUv" class="prev-wall"><span class="icn">&#x2190;</span> PREV</a>
+    --url to extract from example line: http://wallbase.cc/wallpaper/go/2816669/prev?ref=aHR0cDovL3dhbGxiYXNlLmNjL2NvbGxlY3Rpb24vMjkwNTUv
+    local prev_url = string.match(html, '<a href="(http://wallbase%.cc/wallpaper/go/[0-9]+/prev%?ref%=[a-x0-9A-X]+)" class="prev-wall">')
+    if prev_url then
+      table.insert(urls, { url=prev_url })
+    end
+    
+    --example line: <a href="http://wallbase.cc/wallpaper/go/2816669/next?ref=aHR0cDovL3dhbGxiYXNlLmNjL2NvbGxlY3Rpb24vMjkwNTUv" class="next-wall">NEXT <span class="icn">&#x2192;</span></a>
+    --url to extract from example line: http://wallbase.cc/wallpaper/go/2816669/next?ref=aHR0cDovL3dhbGxiYXNlLmNjL2NvbGxlY3Rpb24vMjkwNTUv
+    local next_url = string.match(html, '<a href="(http://wallbase%.cc/wallpaper/go/[0-9]+/next%?ref%=[a-z0-9A-Z]+)" class="prev-wall">')
+    if next_url then
+      table.insert(urls, { url=next_url })
+    end
+  end
+end
+
 wget.callbacks.httploop_result = function(url, err, http_stat)
   -- NEW for 2014: Slightly more verbose messages because people keep
   -- complaining that it's not moving or not working
